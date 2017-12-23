78ffefd8e790d1872df3fd1a904b849c83c9c6be
Sameer Chitley <chitley.sameer@gmail.com>
2017-12-22 23:07:47 -0800

    Project initialized.

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const webpack = require('webpack');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  entry: {
+    app: './app.js',
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: [/node_modules/],
+        use: [{
+          loader: 'babel-loader',
+          options: { presets: ['env'] },
+        }],
+      },
+    ],
+  },
+};
